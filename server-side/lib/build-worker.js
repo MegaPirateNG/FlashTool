bafb887ef8818743722ac3b208b689c46871ac4b
Philipp Andreas <github@smurfy.de>
2013-11-20 22:55:58 +0100

    BuildTool: added clone/fetch switch based on if directory already exists, added de-attached checkout which fixes #12

--- server-side/lib/build-worker.js
+++ server-side/lib/build-worker.js
@@ -7,20 +7,39 @@ var git = require(__dirname + '/git'),
 process.on('message', function(payload) {
     Step(
         function checkExistingHEX() {
-        	fs.exists(payload.hexFile+'.gz', this);
+            fs.exists(payload.hexFile+'.gz', this);
         },
-        function checkCommit(exists) {
-        	  if (exists) {
-	        	  process.send({msg: 'Firmware already built on prev task, fin'});
-              process.send('next');
-        	  	return;
-        	  }
-        	
-        	  process.send({msg: 'git fetch '+payload.config.version['src-repository']});
-            git.fetch(payload.config.version['src-repository'], payload.path, this);
+        function checkSrcPath(exists) {
+            if (exists) {
+                process.send({msg: 'Firmware already built on prev task, fin'});
+                process.send('next');
+                return;
+            }
+            fs.exists(payload.path + '/.git', this);
+        },
+        function checkDir(exists) {
+            if (!exists) {
+                process.send({msg: 'Creating build root: ' + payload.path});
+                fs.mkdir(payload.path, this);
+            } else {
+                return 'exists';
+            }
+        },
+        function checkCommit(error, status) {
+            if (status === 'exists') {
+                process.send({msg: 'git fetch '+payload.config.version['src-repository']});
+                git.fetch(payload.config.version['src-repository'], payload.path, this);
+            } else if (error === null) {
+                process.send({msg: 'git clone '+payload.config.version['src-repository']});
+                git.clone(payload.config.version['src-repository'], payload.path, this);
+            } else {
+                process.send({msg: 'Error with creating the build directory: ' + error});
+                process.send('next');
+                return;
+            }
         },
         function cloned(status) {
-            git.checkout(payload.config.version['src-version'], payload.path, this);
+            git.checkout('origin/'+payload.config.version['src-version'], payload.path, this);
         },
         function prepareMakeFile(status) {
             var makeConfig = '#Config\n' +
@@ -38,11 +57,11 @@ process.on('message', function(payload) {
             fs.writeFile(payload.path + '/config.mk', makeConfig, this);
         },
         function build(status) {
-        	  process.send({msg: 'Build: '+payload.path + '/' + payload.config.version['src-dir']});
+            process.send({msg: 'Build: '+payload.path + '/' + payload.config.version['src-dir']});
             exec('cd ' + payload.path + '/' + payload.config.version['src-dir'] + '; make mpng', this);
         },
         function copyHex(error, stdout, stderror) {
-        	  process.send({msg: 'Copy HEX'});
+            process.send({msg: 'Copy HEX'});
             var srcHex = payload.path + '/_build/' + payload.config.version['src-dir'] + '.hex',
                 dstHex = payload.hexFile;
             fs.copy(srcHex, dstHex, this);
@@ -60,7 +79,7 @@ process.on('message', function(payload) {
             exec('cd ' + hexPath + ';md5sum -b ' + hexName + ' > ' + hexName + '.md5', this);
         },
         function createGz(status) {
-        	  process.send({msg: 'Compress HEX: '+payload.hexFile});
+            process.send({msg: 'Compress HEX: '+payload.hexFile});
             var hexPath = path.dirname(payload.hexFile),
                 hexName = path.basename(payload.hexFile);
             exec('cd ' + hexPath + ';gzip ' + hexName, this);
